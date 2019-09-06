bbf0a929ecf7c7b034b200244fc9c5cbfb90ea7b
Vien_Hong <vienhong@Vien-Hongs-MacBook-Pro.local>
2019-09-06 19:14:11 -0400

    in progress

--- server.js
+++ server.js
@@ -9,8 +9,21 @@ console.log(stripeSecretKey, stripePublicKey);
 
 const express = require("express");
 const app = express();
+const fs = require("fs");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
+app.get("/store", function(req, res) {
+  fs.readFile("./items.json", function(error, data) {
+    if (error) {
+      res.status(500).end();
+    } else {
+      res.render("store.ejs", {
+        items: JSON.parse(data)
+      });
+    }
+  });
+});
 
 app.listen(3000);
